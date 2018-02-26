8daed4fb27d1d6c03a886df8200b1e728e22caf6
biancapatrizavalmeo <biancapatriza@yahoo.com>
2018-02-26 16:51:37 +0800

    adding of presc_details to database

--- public/js/createrx.js
+++ public/js/createrx.js
@@ -1,5 +1,32 @@
 $(document).ready(function(){
+    /* Initialize components*/
     $('.modal').modal();
+    Materialize.updateTextFields();
+
+    /* Get all drugs in the prescription*/
+    $.ajax({
+        type:'GET',
+        url:'http://localhost:3000/api/pres/details',
+        data:{
+            id:1
+        },
+        success: function(response){
+            var prescDetails = response;
+            var detailsData = prescDetails['data'];
+            for (var i = 0; i<detailsData.length; i++){
+                $('ul.collection').append(`
+                    <li class="collection-item dismissable">
+                    <span class="title">
+                    ${detailsData[i].drug_name}
+                    </span>
+                    <p>${detailsData[i].notes}</p>
+                    </li>`);
+            }
+
+        }
+    });
+
+    /* Get all drugs */
     $.ajax({
         type: 'GET',
         url: 'http://localhost:3000/api/drugs',
@@ -8,14 +35,16 @@ $(document).ready(function(){
             var dataArray = drugArray['data'];
             var dataDrug = {};
             for (var i = 0; i < dataArray.length; i++) {
-                dataDrug[dataArray[i].name] = dataArray[i].id; 
+                dataDrug[dataArray[i].name] = null; 
             }
             $('input.autocomplete').autocomplete({
                 data: dataDrug,
-                limit: 5, 
+                limit: 3, 
             });
         }
     });
+
+    /* Selects desired drug */
     $("#btnAddMed").on('click',function(){
         $.ajax({
             type:'GET',
@@ -26,15 +55,28 @@ $(document).ready(function(){
             success:function(response){
                 var addMedArray = response;
                 var addMedData = addMedArray['data'];
-                $('div.collection').html('');
+                // $('div.collection').html('');
                 for(var i = 0; i < addMedData.length; i++){
                     // dataSearch[searchData[i].name] = searchData[i].id;
                     $('ul.collection').append(`
                     <li class="collection-item dismissable">
                     <span class="title">
-                    ${addMedData[i].name}
+                    ${addMedData[i].name}<br/>
+                    ${addMedData[i].id}
                     </span>
                     </li>`);
+                    $.ajax({
+                        type:'POST',
+                        url:'http://localhost:3000/api/pres/details/create',
+                        data:{
+                            "presc_id" : 1,
+                            "drug_id" : addMedData[i].id,
+                            "notes": $('textarea#notes').val()
+                        },
+                        success:function(response){
+                            alert('Medicine successfully added!');
+                        }
+                    });
                 }
                 $('#meds').val('');
                
