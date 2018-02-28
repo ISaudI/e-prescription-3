bce7515a97dbe9f0adc46bc43460f0be8e8793fe
Agca Cruz <32511102+agcacruz@users.noreply.github.com>
2018-02-28 14:32:57 +0800

    notif

--- public/js/notif.js
+++ public/js/notif.js
@@ -1,22 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    //Autocomplete textbox
     $.ajax({
         type: 'GET',
-        url: 'http://localhost:3000/api/doctors',
-        success: function(response){
-            var nameArray = response;
-            var dataArray = nameArray['data'];
-            var dataName = {};
-            for(var i = 0; i < dataArray.length; i++){
-                $('div.collapsible').append(`
-                <li>
-                 <div class="collapsible-header teal lighten-1 active">
-                    <h5>
-                        ${dataArray[i].name}
-                    </h5>
-                 </div>
-                 <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+        url: '/api/notif/patient/1',
+        success: function(response) {
+            let nameArray = response;
+            let dataArray = nameArray['data'];
+            let dataName = {};
+            for (var i = 0; i < dataArray.length; i++) {
+                dataName[dataArray[i].name] = dataArray[i].img;
+                $('div.collection').append(`
+                <li class="collection-item avatar">
+                <img src="images/yuna.jpg" alt="" class="circle">
+                <span class="title">${dataArray[i].id}</span>
+                <p>
+                Actino Type: ${dataArray[i].action_type} <br>
+                Date Created: ${dataArray[i].date_created} <br>
+
+                </p>
                 </li>`);
             }
+            $('input.autocomplete').autocomplete({
+
+                data: dataName,
+                limit: 5,
+            });
         }
     });
 });
