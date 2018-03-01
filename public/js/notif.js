3daf7ed09d07709eb4a5442d8de0b98318e50acd
Agca Cruz <32511102+agcacruz@users.noreply.github.com>
2018-03-01 15:06:10 +0800

    notif

--- public/js/notif.js
+++ public/js/notif.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $.ajax({
         type: 'GET',
+<<<<<<< HEAD
         url: '/api/notif/patient/1',
         success: function (response) {
             let notifArray = response,
@@ -15,6 +16,23 @@ $(document).ready(function(){
                             </span>
                         </div>
                     </li>`);
+=======
+        url: 'http://localhost:3000/api/doctors',
+        success: function(response){
+            var nameArray = response;
+            var dataArray = nameArray['data'];
+            var dataName = {};
+            for(var i = 0; i < dataArray.length; i++){
+                $('div.collapsible').append(`
+                <li>
+                 <div class="collapsible-header teal lighten-1 active">
+                    <h5>
+                        ${dataArray[i].name}
+                    </h5>
+                 </div>
+                 <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+                </li>`);
+>>>>>>> parent of 3df5b97... Merge branch 'reseta-proto' of https://github.com/kendrick004/e-prescription into reseta-proto
             }
         }
     });
