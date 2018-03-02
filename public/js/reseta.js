7c6790fff5d1fadba8f45e0e8c4698b134984214
Agca Cruz <32511102+agcacruz@users.noreply.github.com>
2018-03-02 10:04:11 +0800

    prescriptionlist

--- public/js/reseta.js
+++ public/js/reseta.js
@@ -8,11 +8,31 @@ $(document).ready(function(){
         success: function(response){
             var nameArray = response;
             var dataArray = nameArray['data'];
+            for(var i = 0; i < 1; i++){
+                $('ul.collection').append(`
+                    <li class="collection-item">${dataArray[i].doctor_id}</li>
+                    <li id="profile" class="collection-item"></li>
+                    <li class="collection-item">${dataArray[i].notes}</li>
+                `);
+            }
+        }
+    });
+
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:3000/api/patient',
+        data: {
+            name: "kendrick"
+        },
+        success: function(response){
+            var nameArray = response;
+            var dataArray = nameArray['data'];
             for(var i = 0; i < dataArray.length; i++){
-                $('div.collection').append(`
-                <span class="title">
-                ${dataArray[i].notes}
-                </span><br>`);
+                $('li#profile').append(`
+                    For: ${dataArray[i].name}<br>
+                    Address: ${dataArray[i].country}<br>
+                    ${dataArray[i].city}, ${dataArray[i].state_province}
+                `);
             }
         }
     });
