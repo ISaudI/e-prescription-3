80d9eccdef6ecf58b315a874bd8563791d8bd3ed
Agca Cruz <32511102+agcacruz@users.noreply.github.com>
2018-03-02 11:34:35 +0800

    prescription task-done

--- public/js/reseta.js
+++ public/js/reseta.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
                 $('ul.collection').append(`
                     <li class="collection-item">${dataArray[i].doctor_id}</li>
                     <li id="profile" class="collection-item"></li>
-                    <li class="collection-item">${dataArray[i].notes}</li>
+                    <li id="reseta" class="collection-item">${dataArray[i].notes}<br></li>
                 `);
             }
         }
@@ -36,4 +36,22 @@ $(document).ready(function(){
             }
         }
     });
+
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:3000/api/pres/details',
+        data: {
+            id: "2"
+        },
+        success: function(response){
+            var nameArray = response;
+            var dataArray = nameArray['data'];
+            for(var i = 0; i < dataArray.length; i++){
+                $('li#reseta').append(`
+                    ${dataArray[i].drug_name} ${dataArray[i].notes}
+                `);
+            }
+        }
+    });
+    
 });
