cdbb315ca3e23f7f8cfe6f8c06ca40534109f1f3
biancapatrizavalmeo <biancapatriza@yahoo.com>
2018-03-07 12:21:04 +0800

    completed the CRUD in prescription

--- public/js/prescadd.js
+++ public/js/prescadd.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
         hover: true, // Activate on hover
         gutter: 0, // Spacing from edge 
         alignment: 'right', // Displays dropdown with edge aligned to the left of button
-        stopPropagation: false // Stops event propagation
+        stopPropagation: true // Stops event propagation
       }
       );
     //---end of initialization---
@@ -42,6 +42,7 @@ $(document).ready(function(){
         $('.collection').hide();
         
     }
+
    
     /* Get all drugs */
     $.ajax({
@@ -63,45 +64,67 @@ $(document).ready(function(){
 
     /* Selects desired drug  and add it to the prescription*/
     $("#btnAddMed").on('click',function(){
-        $.ajax({
-            type:'GET',
-            url:'/api/drugs',
-            data: {
-                name: $('input.autocomplete').val()
-            },
-            success:function(response){
-                var addMedArray = response;
-                var addMedData = addMedArray['data'];
-                for(var i = 0; i < addMedData.length; i++){
-                    $.ajax({
-                        type:'POST',
-                        url:'http://localhost:3000/api/pres/details/create',
-                        data:{
-                            "presc_id" :pId,
-                            "drug_id" : addMedData[i].id,
-                            "notes": $('textarea#notes').val()
-                        },
-                        success:function(response){
-                            // alert('Medicine successfully added!');
-                            window.location.reload(true);
-                        }
-                    });
+        if( $('input.autocomplete').val() !=="" && $('input#notes').val() !==""){
+            $.ajax({
+                type:'GET',
+                url:'/api/drugs',
+                data: {
+                    name: $('input.autocomplete').val()
+                },
+                success:function(response){
+                    var addMedArray = response;
+                    var addMedData = addMedArray['data'];
+                    for(var i = 0; i < addMedData.length; i++){
+                        $.ajax({
+                            type:'POST',
+                            url:'http://localhost:3000/api/pres/details/create',
+                            data:{
+                                "presc_id" :pId,
+                                "drug_id" : addMedData[i].id,
+                                "notes": $('textarea#notes').val()
+                            },
+                            success:function(response){
+                                // alert('Medicine successfully added!');
+                                window.location.reload(true);
+                            }
+                        });
+                    }
+                    $('#meds').val('');   
                 }
-                $('#meds').val('');   
-            }
-        });
+            });
+        }else{
+            alert("Enter medicine and notes");        
+        }
+        
     });
 
-
+    /* Getting the details of prescription */
     $(".edit_presdetail").click(function(){
         $this = $(this);
         let detailArray = $this.data("details");
-        // $.ajax({
-        //     type:'GET',
-        //     url:''
-        // })
+        document.getElementById('updatemed').value = `${detailArray.drug_name}`;
+        document.getElementById('detailsId').value = `${detailArray.id}`;
+        document.getElementById('updatenotes').value = `${detailArray.notes}`;
+               
     })
 
+    $("#btnUpdateMed").on('click',function(){
+        if(confirm("Are you sure you want to update this medicine prescribed?")){
+            $.ajax({
+                type: 'POST',
+                url:'/api/pres/details/update',
+                data:{
+                    id: $('input#detailsId').val(),
+                    data:{
+                        notes: $('textarea#updatenotes').val()
+                    }
+                },
+                success: function(){
+                    alert("successfully updated a medicine");
+                }
+            })
+        }
+    })
 
      /* Deleting of prescription details */
      $(".delete_presdetail").click(function(){
