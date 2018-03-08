8b7903a00f2535128f055c3419ace91e9fb00cc2
biancapatrizavalmeo <biancapatriza@yahoo.com>
2018-03-08 15:37:30 +0800

    edited notif routes

--- routes/render/doctors_notif.js
+++ routes/render/doctors_notif.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const notif = require('../../models/notification');
+
 
 router.get('/', function(req, res, next){
-	res.render('doctor/notif',{
-        title: 'Notification',
-        user : req.session.user
-    });
+    notif.getDoctorId(req.session.user.id).then(data=>{
+        res.render('doctor/notif',{
+            title: 'Notification',
+            user : req.session.user,
+            notif: data.data
+        });
+    });    
 });
 
 
--- routes/render/patients_notif.js
+++ routes/render/patients_notif.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const notif = require('../../models/notification');
 
 
-router.get('/notif', function(req, res, next){
+router.get('/', function(req, res, next){
     notif.getPatientId(req.session.user.id).then(data=>{
         res.render('patient/notification',{
             title: 'Notification',
