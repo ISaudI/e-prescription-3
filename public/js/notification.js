4f9d736d6bf95ba385cfb01c53cdd112dd4bf352
Agca Cruz <32511102+agcacruz@users.noreply.github.com>
2018-03-14 15:18:13 +0800

    dashboard

--- public/js/notification.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-        if( $('element.cancel_flag').val() == 1){
-                function changeColor(element){
-                element.style.color = 'red';
-            }
-        }
-});
